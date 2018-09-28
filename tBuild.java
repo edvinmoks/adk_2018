6a4ea4a2792cebb393e5183a5839861264f04633
edvinmoks <edvin.moks@gmail.com>
2018-09-28 15:52:10 +0200

    Add files via upload

--- tBuild.java
+++ tBuild.java
@@ -4,300 +4,143 @@ import java.util.*;
 
 public class tBuild{
 
+    //Create char array of letters for the hashing.
+    static char[] letters = new char[30];
+    static HashMap<String,String> hm = new HashMap<String,String>();
 
 
-    static String[] letters = {" ","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","å","ä","ö"};
+
     public static void main(String[] args){
 
       try {
+        //Fill char array with letter values.
+        int k = 1;
+        for(int i = 0; i < 27; i++){
+          letters[i] = (char)(95 + (k++));
+        }
+        letters[0] = (char)(32); //space
+        letters[27] = (char)(228); //ä (ä first because unix sorts for ASCII values, where ä comes before å)
+        letters[28] = (char)(229); //å
+        letters[29] = (char)(246); //ö
 
 
-
-
-
-
-
-      //BufferedReader buf = new BufferedReader(new FileReader("tempdata.txt"));
-      /*
-      String testw = "ad";
-      String testa = "ade";
-      String testa2 = "adekvat";
-      String testa3 = "adekvata";
-      int testaintw = hashWord(testw);
-      int testaint = hashWord(testa);
-      int testaint2 = hashWord(testa);
-      int testaint3 = hashWord(testa);
-      System.out.println(testaintw);
-      System.out.println(testaint);
-      System.out.println(testaint2);
-      System.out.println(testaint3);
-*/
-      File file = new File("sortedIndexArray.txt");
+      File file = new File("ut.txt");
 
       BufferedReader reader  = new BufferedReader(
       new InputStreamReader(new FileInputStream(file),"ISO-8859-1"));
 
-      //BufferedReader reader = new BufferedReader(new FileReader("test.txt"));
-      // sortedIndexArray
-      //BufferedWriter writerWordList = new BufferedWriter(new FileWriter(new File("wordList.txt")));
 
-/*
-      RandomAccessFile raf = new RandomAccessFile(new File("sortedIndexArray.txt"), "r");
-      raf.seek(11);
-      String apa = raf.readLine();
-      System.out.println("apa: " + apa);
-*/
-      //ArrayList<String> wordList = new ArrayList<String>();
-      //ArrayList<String> positionList = new ArrayList<String>();
-      HashMap<String,String> hm = new HashMap<String,String>();
-      Properties properties = new Properties();
 
-      StringBuilder stringBuilder = new StringBuilder();
-
-            //System.out.println("line: " + line);
-
-          /*
-      long indexCounter = 0;
-      long wordListCount = 0;
-      long sameFirstThreeCount = 0;
-      long positionListCount = 0;
-
-      String tempLine = "";
-      String prevWord = "";
-*/
       BufferedWriter wordList = new BufferedWriter(new FileWriter(new File("wordList.txt")));
       BufferedWriter positionList = new BufferedWriter(new FileWriter(new File("positionList.txt")));
 
       int positionIndex = 0;
       int wordCounter = 0;
-      long indexCounter = 0;
 
-
-      String in = reader.readLine(); //Reads first line from arrayIndex
+      String in = reader.readLine(); //Reads first line from ut.txt
 
       String[] current = in.split("\\s+");
-      String firstThreeCurrent = current[0].substring(0,Math.min(current[0].length(),3)); //Get first three letters of the first word
-      hm.put(String.valueOf(hashWord(firstThreeCurrent)),String.valueOf(indexCounter)); //Add the first word and the correct position to the hashMap
-      indexCounter += in.length()+1; //add the length of the first line to the indexCounter
+
       in = reader.readLine();
       String[] next = in.split("\\s+");
-      //System.out.println("current: " + current[0]);
-      //System.out.println("next: " + next[0]);
-      String firstThreeNext = "";
 
-
-      //indexCounter += in2.length()+1;
-      //System.out.println("indexCounter: "+ indexCounter);
 try{
       while(in != null){
-        wordList.write(current[0]+","+ positionIndex);
+        wordList.write(current[0]+","+positionIndex);
         positionList.write(current[1]);
+
         positionIndex += current[1].length();
         wordCounter = 1;
-        //System.out.println("word: " + word[0]);
-        //System.out.println("indexCounter tBuild: "+ indexCounter);
-        while(current[0].equals(next[0])){ //Checks if current word is the same as the previous word
-          indexCounter += in.length()+1;
+
+        while(current[0].equals(next[0])){ //Checks if current word is the same as the next word
+
           positionList.write(","+next[1]);
-          positionIndex += next[1].length() + 1; //Lägger till längden av numret till postionsListCounter (+1 för komma)
+
+          positionIndex += next[1].length() + 1; //Lägger till längden av siffran till postionsListCounter (+1 för komma)
           wordCounter++;
 
-          //System.out.println("current: "+ current[0]);
-          //System.out.println("next: "+ next[0]);
-          //System.out.println("in: "+ in);
-          /*
-
-          System.out.println("in length: "+ in.length());
-
-          */
-          //  System.out.println("indexCounter: "+ indexCounter);
-          //System.out.println("indexCounter tBuild: "+ indexCounter);
           in = reader.readLine();
-          //System.out.println("in size: "+ in.length());
+
           next = in.split("\\s+");
-          //System.out.println("next word: "+next[0]+" next word size: "+ next[0].length()+" next number size: "+next[1].length());
-          //System.out.println("next size: "+ next[0].length()+next[1].length());
-
-          //System.out.println("indexCounter inside: "+ indexCounter);
-          //System.out.println("current: "+ current[0]);
-          //System.out.println("next: "+ next[0]);
-        }
-        //System.out.println("indexCounter: "+ indexCounter);
-        firstThreeNext = next[0].substring(0,Math.min(next[0].length(),3)); //Get first three letters of the word
-        firstThreeCurrent = current[0].substring(0,Math.min(current[0].length(),3)); //Get first three letters of the word
-        //System.out.println("firstThreeNext: "+ firstThreeNext);
-        //System.out.println("indexCounter outside: "+ indexCounter);
-
-        if(hashWord(firstThreeNext) != hashWord(firstThreeCurrent)){ //kollar om ord är annorlunda från den andra, isåfall lägg in i hashtabell
-
-          hm.put(String.valueOf(hashWord(firstThreeNext)),String.valueOf(indexCounter)); //Lägger till unika ord
-
         }
 
-        positionList.write("\n");
-        //System.out.println("wordList: "+ wordList);
-
-    //System.out.println("positionList: "+ positionList);
-
-    //System.out.println("indexCounter: "+ indexCounter);
+      positionList.write("\n");
       positionList.flush();
       positionIndex++; //For the new line
 
       wordList.write(","+wordCounter+"\n");
       wordList.flush();
-      indexCounter += in.length()+1;
+
       current = next;
       in = reader.readLine();
       if(in==null) {
-          break;
+        positionList.write(current[1]);
+        wordList.write(current[0]+","+positionIndex+","+"1");
+        break;
       }
       next = in.split("\\s+");
 
     } // END WHILE
+
+    reader.close();
+    wordList.close();
+    positionList.close();
+
+    File file2 = new File("wordList.txt");
+    BufferedReader reader2  = new BufferedReader(
+    new InputStreamReader(new FileInputStream(file2),"ISO-8859-1"));
+    in = reader2.readLine();
+    current = in.split(",");
+    Integer counter = 0;
+    String[] previous = {"z","z","z"}; //Initialize with letters to avoid exception.
+
+
+
+
+    boolean go = true;
+    while(go){
+
+      if(hashWord(current[0])!=hashWord(previous[0])){ //kollar om ord är annorlunda från den andra, isåfall lägg in i hashtabell
+
+        hm.put(String.valueOf(hashWord(current[0])),String.valueOf(counter)); //Lägger till unika ord
+
+      }
+      counter += in.length()+1;
+      previous = current;
+      in = reader2.readLine();
+      //System.out.println("in: "+ in);
+      if(in==null){
+        break;
+      }
+
+      current = in.split(",");
+
+    }
+
 }
 catch(NullPointerException ex){
   System.out.println("Exception!: "+"\n"+"current: "+current[0]+"\n"+"next: "+next[0]);
 
-  //System.out.println("positionList: "+ positionList);
-
-
-
 }
-  //System.out.println("indexCounter: "+ indexCounter);
-
-    //System.out.println("wordList: "+wordList);
-            //wordList.add(word[0] + " " + wordCount);
-            //String tempLine = word+" "+Integer.toString(indexCounter)+"\n";
-    //positions.add(builder.toString());
-    //System.out.println("first pos: "+positions);
-
-    //System.out.println("second pos: "+positions);
-    //wordCount = 0;
-    //wordList.add(word[0] + " " + wordCount);
-
       reader.close();
       wordList.close();
       positionList.close();
 
-      /*
-      writerWordList.flush();
-      writerWordList.close();
-      BufferedWriter positionList = new BufferedWriter(new FileWriter(new File("positionList.txt")));
-      builder = new StringBuilder();
-      builder.append(",");		// Add a comma to our stringbuilder...
-      builder.append("1");	// And the new position it's found at.
-      builder.append(",");
-      builder.append("2");
-      builder.append(",");
-      builder.append("3");
-      positions.add(builder.toString());
-      builder = new StringBuilder();
-      builder.append(",");		// Add a comma to our stringbuilder...
-      builder.append("4");	// And the new position it's found at.
-      builder.append(",");
-      builder.append("5");
-      builder.append(",");
-      builder.append("6");test
-      positions.add(builder.toString());
-      System.out.println("positions array: "+positions);
-      for (String record: positions) {
-        positionList.write(record);
-        positionList.write("\n");
-   }
-    positionList.close();
-*/
 
+      Properties properties = new Properties();
       for (HashMap.Entry<String,String> entry : hm.entrySet()) {
 
           properties.put(entry.getKey(), entry.getValue());
           }
 
       properties.store(new FileOutputStream("data.properties"), null);
-/*
-      indexCounter= 0;
-      long st = System.currentTimeMillis();
-
-      String fw = reader.readLine();
-      String[] fw1 = fw.split(" ");
-      String fw2 = fw1[0];
-      hm.put(fw2,indexCounter);
-      while((line = reader.readLine()) != null ){
-        indexCounter++;
-        String[] tempWord = line.split(" ");
-        String firstThree = tempWord[0].substring(0,Math.min(tempWord[0].length(),3));
-        if(!firstThree.equals(fw2)){
-
-          hm.put(firstThree,indexCounter);
-          fw2= firstThree;}
-
-
-
-      }
-      long et = System.currentTimeMillis();
-      System.out.println(et-st);
-
-*/
-
-/*
-
-      //Skapar en array av alla index i filen L;
-      ArrayList<String> tokenizer = new ArrayList<String>();
-      BufferedReader reader2 = new BufferedReader(new FileReader("ut.txt"));
-      //BufferedWriter writer2 = new BufferedWriter(new FileWriter(new File("indexArray.txt")));
-      String line2;
-      int counter2 = 0;
-      String[] tempWord2;
-      while((line2 = reader2.readLine()) != null ){
-        tempWord2 = line2.split(" ");
-
-        tokenizer.add(tempWord2[1]);
-
-      }
-      //System.out.println("tokenizer: "+tokenizer.get(0));
-
-
-
-      reader2.close();
-
-*/
-
-
-
-      //Läsa position, skapa mening
-/*
-      RandomAccessFile raf = new RandomAccessFile("/info/adk18/labb1/korpus", "r");
-      int tf = Integer.parseInt(tokenizer.get(0));
-      raf.seek(tf-15);
-      byte[] tp = {raf.readByte()};
-      String bs = new String(tp,"ISO-8859-1");
-      for(int i=0; i <=30; i++){
-        byte[] tpa = {raf.readByte()};
-        bs += new String(tpa,"ISO-8859-1");
-
-      };
-      System.out.println(bs);
-*/
-
-      //Write hashMap to file
-
-
-
-
-
-
-
-
-    }
-      catch (IOException e){
-                System.out.println("there was an error");
-
-  }
-
-
 
 }
+      catch (IOException e){
+                System.out.println("there was an error");
+  }
+}//END MAIN
 
-//END MAIN
 public static int hashWord(String word) {
   int i = 0;
   int hashValue = 0;
@@ -308,12 +151,10 @@ public static int hashWord(String word) {
   return hashValue;
 }
 
-/**
-* Returns the corresponding value to each letter.
-*/
+//Returns the corresponding value of each letter.
 public static int getLetterValue(String letter) {
   for(int i = 0; i < letters.length; i++) {
-    if(letter.equals(letters[i])) {
+    if(letter.equals(Character.toString(letters[i]))) {
       return i;
     }
   }
--- tKonkordans.java
+++ tKonkordans.java
@@ -5,17 +5,22 @@ import java.util.*;
 
 public class tKonkordans{
 
+    static char[] letters = new char[30];
 
-    private static String[] letters = {" ","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","å","ä","ö"};
     public static void main(String[] args){
-
-      try {
+        try{
+        //Fill char array with letter values.
+        int k = 1;
+        for(int i = 0; i < 27; i++){
+          letters[i] = (char)(95 + (k++));
+        }
+        letters[0] = (char)(32); //space
+        letters[27] = (char)(228); //ä (ä first because unix sorts for ASCII values, where ä comes before å)
+        letters[28] = (char)(229); //å
+        letters[29] = (char)(246); //ö
 
         String searchWord = "";
         String hashedSearchWord = "";
-
-
-
 
         if (args.length == 0) {
     			System.out.println("No input.");
@@ -23,6 +28,7 @@ public class tKonkordans{
     			//System.out.println(args[0]);
           searchWord = args[0].toLowerCase();
           hashedSearchWord = String.valueOf(hashWord(searchWord));
+          //System.out.println("hashedSearchWord: "+hashedSearchWord);
     		}
         // Läs in hashmapen som genererats i build.java
         Map<String, String> hm = new HashMap<String, String>();
@@ -32,317 +38,120 @@ public class tKonkordans{
             hm.put(key, properties.get(key).toString());
           }
 
-
         String position = hm.get(hashedSearchWord); //Get the position of the search word in the indexArray.
-        //System.out.println("position: " + position);
+
         Integer indexPosition = Integer.parseInt(position);
+        System.out.println("indexPosition: "+indexPosition);
+        RandomAccessFile rafWord = new RandomAccessFile(new File("wordList.txt"), "r");
+        rafWord.seek(indexPosition);
+        String foo = rafWord.readLine();
+        String[] word = foo.split(",");
 
-        //indexPosition = (indexPosition)/2; //Adjust value for position
-        //System.out.println("indexPosition: " + indexPosition);
-        ArrayList<String> ordLista = new ArrayList<String>();
+        String firstThreeSearchWord = searchWord.substring(0,Math.min(searchWord.length(),3)); //get first three letters from search word
+        String firstThreeCurrent = word[0].substring(0,Math.min(word[0].length(),3)); //get first three letters from first three letters of the first ocurrence of the searchword.
 
-        String tempLine = "";
-        String line2 = "";
-/*
-        BufferedReader reader2 = new BufferedReader(new FileReader("wordList.txt"));
-        //line2 = reader2.readLine(); //remove first bad line
-        while((line2 = reader2.readLine()) != null ){
-          //tempLine = line2.split(" ");
-          ordLista.add(line2);
-        }apa.length()
-        System.out.println(ordLista);
-        //System.out.println(ordLista.get(indexPosition));
-        reader2.close();
-
-        //Läs in indexArray skapad av tokenizer.c
-        BufferedReader reader = new BufferedReader(new FileReader("sortedIndexArray.txt"));
-        //Läs in wordList = ord - antal ord
-
-
+        boolean found = false;
         String line = "";
-        for (int i = 0; i < indexPosition+1; i++){
 
-          line = reader.readLine();
-          //System.out.println("i: "+i);
-          //System.out.println("line: "+line);
+        while(firstThreeSearchWord.equals(firstThreeCurrent)){
 
-        }
-
-        //String[] theLine = line.split(" ");
-        //String korpusIndex = theLine[1];
-        //System.out.println("line: "+line);
-        reader.close();
-*/      String apa = "";
-        System.out.println("indexPosition: " + indexPosition);
-        RandomAccessFile raf = new RandomAccessFile(new File("sortedIndexArray.txt"), "r");
-        raf.seek(indexPosition);
-        apa = raf.readLine();
-/*
-        while(apa.length() < 1){
-          System.out.println("loop!");
-          indexPosition++;
-          raf.seek(indexPosition);
-          apa = raf.readLine();
-        }
-*/
-        System.out.println("apa: "+apa);
-        //System.out.println("apa length2: "+apa.length());
-        //System.out.println("apa: "+apa);
-
-
-/*
-        BufferedReader reader2 = new BufferedReader(new FileReader("wordList.txt"));
-        line = reader2.readLine();
-        System.out.println("wordList: " + line);
-        line = reader2.readLine();
-        System.out.println("wordList: " + line);
-        line = reader2.readLine();
-        System.out.println("wordList: " + line);
-        line = reader2.readLine();
-        System.out.println("wordList: " + line);
-*/
-
-
-
-
-
-
-
-
-/*
-        //Läsa position, skapa mening
-        RandomAccessFile raf = new RandomAcscessFile("test.txt", "r");
-        raf.seek(0);
-        byte[] bytes = new byte[1];
-        raf.read(bytes);
-        raf.close();
-        System.out.println(new String(bytes));
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
-
-                //Läsa position, skapa mening
-
-                RandomAccessFile raf = new RandomAccessFile("/info/adk18/labb1/korpus", "r");
-                int tf = Integer.parseInt(tokenizer.get(0));
-                raf.seek(tf-15);
-                byte[] tp = {raf.readByte()};
-                String bs = new String(tp,"ISO-8859-1");
-                for(int i=0; i <=30; i++){
-                  byte[] tpa = {raf.readByte()};
-                  bs += new String(tpa,"ISO-8859-1");
-
-                };
-                System.out.println(bs);
-
-        */
-
-
-
-/*
-         Map.Entry<String,String> entry = hm.entrySet().iterator().next();
-         String key = entry.getKey();
-         String value = entry.getValue();
-         System.out.println(key);
-         System.out.println(value);
-         */
-         //String searchWordHash = String.valueOf(hashWord(searchWord));
-         //System.out.println(searchWordHash);
-
-
-        //BufferedReader buf = new BufferedReader(new FileReader("tempdata.txt"));
-        /*
-        BufferedReader reader = new BufferedReader(new FileReader("ut.txt"));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(new File("hashtable.txt")));
-
-        StringBuilder builder = new StringBuilder();
-
-        String line = reader.readLine();
-        String[] firstLine = line.split(" ");
-        String word = firstLine[0];
-        int indexCounter = 0;
-
-        String tempLine = word+" "+Integer.toString(indexCounter)+"\n";
-        System.out.println(tempLine);
-        writer.write(word+" "+Integer.toString(indexCounter));
-
-
-                reader.close();
-                writer.flush();
-                writer.close();
-
-*/
-
-
-        //HashMap<String,Integer> hm = new HashMap<String,Integer>();
-/*
-        while(line != null){
-          indexCounter++;
-          String[] tempWord = line.split(" ");
-          String firstThree = tempWord[0].substring(0,Math.min(tempWord[0].length(),3));
-          if(!firstThree.equals(word)){ //kollar om ord är annorlunda från den andra, isåfall lägg in i hashtabell
-            //x=hashfunktion(firstthree).
-            tempLine = firstThree+" "+Integer.toString(indexCounter)+"\n"; //format för ABC - INDEX, newline
-            hm.put(firstThree,indexCounter);
-            //.seek(x)
-            writer.write(tempLine); // skriver till filen
-            word = firstThree;
+          if(word[0].equals(searchWord)){
+            found = true;
+            break;
           }
-          line = reader.readLine();
-
+          line = rafWord.readLine();
+          word = line.split(",");
+          firstThreeCurrent = word[0].substring(0,Math.min(word[0].length(),3));
         }
-        */
-/*
-        indexCounter= 0;
-        long st = System.currentTimeMillis();
 
-        String fw = reader.readLine();
-        String[] fw1 = fw.split(" ");
-        String fw2 = fw1[0];
-        hm.put(fw2,indexCounter);
-        while((line = reader.readLine()) != null ){
-          indexCounter++;
-          String[] tempWord = line.split(" ");
-          String firstThree = tempWord[0].substring(0,Math.min(tempWord[0].length(),3));
-          if(!firstThree.equals(fw2)){
-
-            hm.put(firstThree,indexCounter);
-            fw2= firstThree;}
-
-
-
+        if(!found){
+          System.out.println("The word does not exist in the database.");
+          System.exit(0);
         }
-        long et = System.currentTimeMillis();
-        System.out.println(et-st);
 
-*/
+        System.out.println("There is "+ word[2] +" occurences of the word "+word[0]);
+        boolean printAll = true;
+        int wordCount = Integer.parseInt(word[2]);
 
-/*
-
-        //Skapar en array av alla index i filen L;
-        ArrayList<String> tokenizer = new ArrayList<String>();
-        BufferedReader reader2 = new BufferedReader(new FileReader("ut.txt"));
-        String line2;
-        int counter2 = 0;
-        String[] tempWord2;
-        while((line2 = reader2.readLine()) != null ){
-          tempWord2 = line2.split(" ");
-          tokenizer.add(tempWord2[1]);
-
-        }
-        System.out.println("tokenizer: "+tokenizer.get(0));
-        reader2.close();
-*/
-
-
-
-
-
-
-
+        if(wordCount > 25){
+          printAll = false;
+          System.out.println("Since there is more than 25 occurences of the word, do you want to print all the sentences where it occurs? [y/n]");
+          Scanner scanner = new Scanner(System.in);
+          String answer = scanner.nextLine();
+          if(answer.equals("y")){
+            printAll = true;
           }
-
-
-        catch (IOException | NumberFormatException e){
-          System.out.println("The word does not exist in database");
         }
 
+        RandomAccessFile rafPos = new RandomAccessFile(new File("positionList.txt"), "r");
+        int pos = Integer.parseInt(word[1]);
+        rafPos.seek(pos);
+        String tempLine = rafPos.readLine();
+        String[] positions = tempLine.split(",");
+
+        int wordLength = word[0].length();
+        int end = 60+wordLength;
+        String bas = "";
+        String bs = "";
+        RandomAccessFile rafKorp = new RandomAccessFile("korpus.txt", "r");
 
 
-    }
+        if(printAll){
+        for(int i=0; i < positions.length; i++){
+          pos = Integer.parseInt(positions[i]);
+          pos = pos - 30;
 
-    //END main
+          if((pos)<0){
+            while((pos)<0){
+              pos++;
+            }
+          }
+          if((pos+end)>rafKorp.length()){
+            while((pos+end)>rafKorp.length()){
+              end--;
+            }
+          }
+          rafKorp.seek(pos);
 
-    public static int hashWord(String word) {
-      int i = 0;
-      int hashValue = 0;
-      while(i < word.length() && i < 3) {
-        hashValue += Math.pow(30,2-i) * getLetterValue(word.substring(i,i+1));
-        i++;
-      }
-      return hashValue;
-    }
-
-    /**
-    * Returns the corresponding value to each letter.
-    */
-    public static int getLetterValue(String letter) {
-      for(int i = 0; i < letters.length; i++) {
-        if(letter.equals(letters[i])) {
-          return i;
+            for(int j=0; j < end; j++){
+              byte[] tpa = {rafKorp.readByte()};
+              bs = new String(tpa,"ISO-8859-1");
+              bs = bs.replaceAll("\n", " ");
+              bas += bs;
+            }
+          System.out.println(bas);
+          bas = "";
+          end = 60+wordLength;
         }
-      }
-      return 0;
-    }
-/*
-    public int binarySearch(String arr[], String x)
-    {
-        int l = 0, r = arr.length - 1;
-        while (l <= r)
-        {
-            int m = l + (r-l)/2;
-
-            // Check if x is present at mid
-            if (arr[m] == x)
-                return m;
-
-            // If x greater, ignore left half
-            if (arr[m] < x)
-                l = m + 1;
-
-            // If x is smaller, ignore right half
-            else
-                r = m - 1;
         }
 
-        // if we reach here, then element was
-        // not present
-        return -1;
-    }
-
-*/
 }
-
-/*
-public class HashTable{
-
-  public static void HashFile(){
+    catch (IOException e){
+          System.out.println("There was an error loading files");
+        }
 
 
-    BufferedReader buf = new BufferedReader(new FileReader("tempdata.txt"));
-    String line = buf.readLine();
-    BufferedWriter writer = new BufferedWriter(new FileWriter("hashtable.txt"));
-    String[] firstLine = line.split(" ");
 
-    int indexCounter = 1;
-    writer.write(firstLine[0]+" "+Integer.toString(indexCounter));
-    while(buf.readline != null){
-
-
+    }    //END main
+  //Creates a unique hash value for the first three letters of every word.
+  public static int hashWord(String word) {
+    int i = 0;
+    int hashValue = 0;
+    while(i < word.length() && i < 3) {
+      hashValue += Math.pow(30,2-i) * getLetterValue(word.substring(i,i+1));
+      i++;
     }
+    return hashValue;
+  }
 
-
-
+  //Returns the corresponding value of each letters for the char array.
+  public static int getLetterValue(String letter) {
+    for(int i = 0; i < letters.length; i++) {
+      if(letter.equals(Character.toString(letters[i]))) {
+        return i;
       }
-*/
+    }
+    return 0;
+  }
+
+}
